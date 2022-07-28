034d7ed0e4acf8663af76e9dc95dca29a80e4c7c
Niaz264 <82967650+Niaz264@users.noreply.github.com>
2022-07-28 22:44:56 +0600

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -16,6 +16,7 @@ from json import loads as jsnloads
 from lk21 import Bypass
 from lxml import etree
 from time import sleep
+import re
 from cfscrape import create_scraper
 from bs4 import BeautifulSoup
 from base64 import standard_b64encode
